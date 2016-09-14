9a61d97859d6c2b30460e8c87bfaac21f8533137
amdudda <tk0654wm@minneapolis.edu>
2016-09-14 18:54:50 -0500

    Push - moved calculator methods to individual functions so we can build tests just for those.  We have 2 passing tests!

--- calculator_Operator.py
+++ calculator_Operator.py
@@ -2,21 +2,26 @@
 
 
     #Lets create a calculator object that add, subtract, divide and multiply 2 values
-class Calculator:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
+# class Calculator:
+#     def __init__(self, value1, value2):
+#         self.value1 = value1
+#         self.value2 = value2
 
+# TODO code does not work with float numbers :(
+def addition(value1, value2):
+    if not (isinstance(value1,str) or isinstance(value2,str)):
+        return value1 + value2
+    else: return None
 
-    def addition(self):
-        return self.value1 + self.value2
+def substraction(value1, value2):
+    if not (isinstance(value1,str) or isinstance(value2,str)):
+        return value1 - value2
+    else: return None
 
-    def substraction(self):
-        return self.value1 - self.value2
+def multiply(value1, value2):
+    return value1 * value2
 
-    def multiply(self):
-        return self.value1 * self.value2
+def divide(value1, value2):
+    return value1 / value2
 
-    def divid(self):
-        return self.value1 / self.value2
-
+# print(addition(4.7,3.5))
---
+++ test_calculator.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from calculator_Operator import *
+
+class TestCalculator(TestCase):
+    # for testing purposes, we want to verify the calculator object
+
+    def test_addition(self):
+        # verify integer math works
+        self.assertTrue(addition(4,5)==9)
+        # verify float math works - call 7 places close enough
+        self.assertAlmostEqual(addition(4.7,3.3),8.0,delta=.000001)
+        # verify it doesn't concatenate strings
+        self.assertIsNone(addition("alpah","beta"))
+
+    def test_substraction(self):
+        # verify integer math works
+        self.assertTrue(substraction(4, 5) == -1)
+        # verify float math works - call 7 places close enough
+        self.assertAlmostEqual(substraction(4.7, 3.3), 1.4, delta=.000001)
+        # verify it doesn't concatenate strings
+        self.assertIsNone(substraction("alpah", "beta"))
+
+    def test_multiply(self):
+        self.fail()
+
+    def test_divid(self):
+        self.fail()
