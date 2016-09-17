ee43f1849a3f557e40433468182a1bca8595f899
amdudda <tk0654wm@minneapolis.edu>
2016-09-16 21:16:44 -0500

    Move is_whole_number() from main to validator

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from calculator_Operator import *
-
+from validator import *
 
 def operation_menu(calculation):
 
@@ -33,28 +33,6 @@ def operation_menu(calculation):
         elif menu_choice == 6:
             return False
             break
-
-
-
-
-
-def is_whole_number(message, valid_range = None):
-    while True:
-        user_input = input('{} :'.format(message))
-        try:
-            number = int(user_input)
-        except ValueError:
-            print("Please enter a correct Value: ")
-            continue
-        if valid_range and number not in valid_range:
-            _min = min(valid_range)
-            _max = max(valid_range)
-            print('You must enter a number from {} to {}.'.format(_min, _max))
-            continue
-
-        # All tests passed
-        return number
-
 
 
 def main():
--- validator.py
+++ validator.py
@@ -8,3 +8,21 @@ def isNumeric(tocheck):
         return True
     except:
         return False
+
+# verify input is a whole number
+def is_whole_number(message, valid_range = None):
+    while True:
+        user_input = input('{} :'.format(message))
+        try:
+            number = int(user_input)
+        except ValueError:
+            print("Please enter a correct Value: ")
+            continue
+        if valid_range and number not in valid_range:
+            _min = min(valid_range)
+            _max = max(valid_range)
+            print('You must enter a number from {} to {}.'.format(_min, _max))
+            continue
+
+        # All tests passed
+        return number
