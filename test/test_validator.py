6aac62aa9d158fc5e9a11420877a2d85c5acd9e5
YannGitHubjava <yanirash@yahoo.fr>
2016-09-17 12:40:28 -0700

    added some tests for empty inputs

--- test/test_validator.py
+++ test/test_validator.py
@@ -13,6 +13,8 @@ class TestValidator(TestCase):
         self.assertTrue(isNumeric(-7008523189.28158436))
         # test that strings return false
         self.assertFalse(isNumeric("string"))
+        # test that an empty input return false
+        self.assertFalse(isNumeric(""))
 
     def test_is_whole_number(self):
         # check that whole numbers with no ranges pass, including large ones
@@ -32,6 +34,7 @@ class TestValidator(TestCase):
         self.assertFalse(is_whole_number(13.333, range(-10, 5)))
         # and check that strings return false
         self.assertFalse(is_whole_number("string"))
+        self.assertFalse(is_whole_number(""))
         # and verify it can cope with stringlike numbers
         self.assertTrue(is_whole_number("7"))
         self.assertFalse(is_whole_number("7.5"))
