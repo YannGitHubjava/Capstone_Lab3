a2deb10a042c97f1d4db8172373d541a5dbc4a80
amdudda <tk0654wm@minneapolis.edu>
2016-09-16 22:14:40 -0500

    Comment cleanup.

--- validator.py
+++ validator.py
@@ -13,17 +13,12 @@ def is_whole_number(number,valid_range = None):
     try:
         number = int(number)
         # if parsing as integer does not produce an error, check if it's in range if a range has been given
-        if valid_range and number not in valid_range: # number >= min(valid_range) and number <= max(valid_range):
+        if valid_range and number not in valid_range:
             _min = min(valid_range)
             _max = max(valid_range)
-            # print('You must enter a number from {} to {}.'.format(_min, _max))
             return False
-        # otherwise check that number and int are equal
+        # if we get to here, we can return true
         return True
     except ValueError:
         # also return False if an integer is not passed
         return False
-
-
-        # All tests passed
-        # return number
