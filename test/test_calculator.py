f98234de5a4e3a2400c491af6a51f9fbe2c97a79
amdudda <tk0654wm@minneapolis.edu>
2016-09-18 15:13:53 -0500

    Comment cleanup

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -12,14 +12,11 @@ class TestCalculator(TestCase):
         # verify float math works - call 7 places close enough
         self.assertAlmostEqual(Calculator(4.7,3.3).addition(),8.0,delta=.000001)
 
-
     def test_substraction(self):
         # verify integer math works
         self.assertTrue(Calculator(4, 5).substraction() == -1)
         # verify float math works - call 7 places close enough
         self.assertAlmostEqual(Calculator(4.7, 3.3).substraction(), 1.4, delta=.000001)
-        # verify it doesn't concatenate strings
-        #self.assertIsNone(Calculator("alpha", "beta").substraction())
 
     def test_multiply(self):
         # verify integer math works
